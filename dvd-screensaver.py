fb655eca760081d082f62573297853c6bcce5763
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2021-10-17 11:45:37 -0700

    Add various improvements and bug fixes, add automated build workflow

--- dvd-screensaver.py
+++ dvd-screensaver.py
@@ -1,197 +1,198 @@
 #----------------------------------------------------------------------#
 # Customization Settings
 
-# Number of items bouncing around on screen.
-numberOfImages = 1
+# Ignore the config file and use these settings (for debugging)
+ignore_config_file = False
 
-# Style of image. Any number from 1 to 4 [3 and 4 are broken at the moment]
-imageType = '1'
+# These settings only apply if ignore_config_file is set to True or the config file cannot be found
+# Number of items bouncing around on screen
+image_count = 1
 
-# Color type.
-# The first value is the colors used. 'preset' or 'random'
-# The second value is the color inversion. 'normal' or 'inverted'
-# When set to 'inverted' the colored areas become clear and vice versa.
-colorType = ['preset', 'normal']
+# Style of image, can be either '1' or '2'
+style = '1'
 
-# The maximum width and height of the image
-# Measured in pixels
-imageSize = [300, 300]
+# Color type, can be 'preset', 'random', or 'constant'
+color_type = 'preset'
 
-# Color of the background. Standard RGB value.
-backgroundColor = (0, 0, 0)
+# Transparency inversion, can be either 'normal' for 'inverted'
+# If set to 'inverted' the colored areas of the image become clear and vice versa
+transparency_inversion = 'normal'
 
-# Amount of delay after every loop cycle. Larger value = slower speed.
-# Measured in seconds.
-refreshSpeed = 1/500
+# RGBA value for the image(s) if color_type is set to 'constant'
+rgba = (255, 255, 255, 255)
 
-# The speed multiplier of the image on 'x' and 'y'.
-# The first value is the speed type. 'random' or 'constant'
-# When set to 'random' the next two integers are the range for the speed to be chosen from.
-# When set to 'constant' the next two integers are the speed for 'x' and 'y'.
-# Can only be a whole integer greater than zero.
-# '1' = 1x speed, '2' = 2x speed, '0.5' = 0.5x speed, etc.
-# Measured in pixels.
-speedType = ['constant', 1, 1]
+# The maximum width and height of the image in pixels
+image_size = (300, 300)
 
-# Width and height of the screen in pixels.
-# Set to your displays resolution.
-width, height = 1920, 1080
+# RGB color of the background
+background_color = (0, 0, 0)
+
+# Refresh speed of the display loop in updates per second
+# Larger value = faster speed
+refresh_speed = 60
+
+# Speed type, can be 'random' or 'constant'
+speed_type = 'constant'
+
+# If speed_type is set to 'constant' the integers are the x and y speed of the image in pixels
+# If speed_type is set to 'random' the integers are the range for the speed to be chosen from
+# '1' = 1x speed, '2' = 2x speed, '0.5' = 0.5x speed, etc
+speed = [2, 2]
+
+# Width and height of the screen in pixels
+# Set to your displays resolution
+display_width, display_height = 1920, 1080
+
+# Path to the config file
+config_file_path = r'%APPDATA%/LemonPi314/dvd-screensaver/config.json'
 #----------------------------------------------------------------------#
 
-import sys, os , time, json
-
-def DisablePrint():
-    sys.stdout = open(os.devnull, 'w')
-
-def EnablePrint():
-    sys.stdout = sys.__stdout__
-
-DisablePrint()
+import sys
+import os
+import time
+import json
+# Disable standard output so the pygame message doesnt show up
+s = sys.stdout
+sys.stdout = open(os.devnull, 'w')
 import pygame
-EnablePrint()
+# Return standard output to normal
+sys.stdout = s
 from PIL import Image
-from pathlib import Path
 from ctypes import windll
-from random import randrange
-from gui import Menu
+from random import choice, randrange, uniform
+from config import menu
 
-def ResourcePath(relativePath):
-    try:
-        basePath = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+resource_dir = os.path.dirname(__file__)
+config_file_path = os.path.expandvars(config_file_path)
+args = [arg.lower() for arg in sys.argv]
+if '/s' not in args and '-s' not in args and '/p' not in args and '-p' not in args:
+    menu(config_file_path)
+    sys.exit()
 
-    except:
-        basePath = os.path.abspath('.')
 
-    return os.path.join(basePath, relativePath)
-    
-resourceDir = Path(ResourcePath('dvd-screensaver.py')).parent
-workingDir = Path(os.getcwd())
+def resource_path(relative_path: str) -> str:
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(__file__))
+    return os.path.join(base_path, relative_path)
 
-if '/c' in sys.argv:
-    Menu(os.path.join(resourceDir, 'config.json'))
-    sys.exit(0)
 
-class image:
-    def __init__(self):
-        global imageType
-        if speedType[0] == 'random':
-            self.imageSpeed = [randrange(speedType[1], speedType[2]) / 1.0, randrange(speedType[1], speedType[2]) / 1.0]
-            if randrange(0, 2) == 1:
-                self.imageSpeed[0] = -self.imageSpeed[0]
+class ScreensaverImage:
+    def __init__(self, screen: pygame.Surface, style: str, size: tuple[int, int], speed_type: str, speed: list[float, float], color_type: str, transparency_inversion: str, rgba: list[int, int, int, int]):
+        self.screen = screen
+        self.style = style
+        self.size = size
+        self.speed_type = speed_type
+        self.speed = speed
+        self.color_type = color_type
+        self.transparency_inversion = transparency_inversion
+        self.rgba = rgba
 
-            if randrange(0, 2) == 1:
-                self.imageSpeed[1] = -self.imageSpeed[1]
-
-        elif speedType[0] == 'constant':
-            self.imageSpeed = [speedType[1] / 1.0, speedType[2] / 1.0]
-
-        #if resourceDir.name.startswith('dvd'):
-        self.imageDir = os.path.join(resourceDir, 'images', '')
-
-        #else:
-        #    self.imageDir = os.path.join(resourceDir, 'dvd-screensaver', 'images', '')
-            
-        imageType = os.path.join(imageType, '')
-        if colorType[1] == 'true' or colorType[1] == 'inverted':
-            colorType[1] = 'inverted/'
-
+        if self.speed_type == 'random' and self.speed[0] != self.speed[1]:
+            self.speed = [round(uniform(min(self.speed), max(self.speed)), 1), round(uniform(min(self.speed), max(self.speed)), 1)]
         else:
-            colorType[1] = 'normal/'
+            self.speed = [self.speed[0], self.speed[1]]
+        if randrange(0, 2) == 1:
+            self.speed[0] = -self.speed[0]
+        if randrange(0, 2) == 1:
+            self.speed[1] = -self.speed[1]
 
-        self.imageDirCurrent = os.path.join(self.imageDir, 'current-image', 'current-image.png')
-        self.imageFiles = os.listdir(self.imageDir + imageType + colorType[1]) 
-        self.PickImage()
-        self.imageRect = self.imageDisplayed.get_rect()
-        self.x = randrange(0, width-self.imageRect.width)
-        self.y = randrange(0, height-self.imageRect.height)
-        if colorType[1] == 'constant':
-            self.ChangeColor(colorType, imageType, rgba)
+        self.image_dir = resource_path(os.path.join('images', self.style, self.transparency_inversion))
+        self.images = [Image.open(os.path.join(self.image_dir, file)) for file in os.listdir(self.image_dir)]
+        for image in self.images:
+            image.thumbnail(self.size)
+        self.pick_image()
+        self.constant_color = False
+        self.pygame_image = pygame.image.fromstring(self.current_image.tobytes(), self.current_image.size, self.current_image.mode)
+        self.rect = self.pygame_image.get_rect()
+        self.rect.left = randrange(0, display_width - self.rect.width)
+        self.rect.top = randrange(0, display_height - self.rect.height)
+        self.change_color()
 
+    def pick_image(self):
+        self.current_image = choice(self.images[:-1])
+        self.images.append(self.images.pop(self.images.index(self.current_image)))
+
+    def change_color(self):
+        if self.color_type == 'preset':
+            self.pick_image()
         else:
-            self.ChangeColor('random', imageType)
-
-    def PickImage(self):
-        randomFile = self.imageFiles[randrange(0, len(self.imageFiles) - 1)]
-        image = Image.open(self.imageDir + imageType + colorType[1] + randomFile)
-        self.imageFiles.insert(len(self.imageFiles), self.imageFiles.pop(self.imageFiles.index(randomFile)))
-        image.thumbnail(imageSize)
-        image.save(self.imageDirCurrent)
-        self.imageDisplayed = pygame.image.load(self.imageDirCurrent)
-
-    def ChangeColor(self, colorType, imageType, rgba = [randrange(0, 255), randrange(0, 255), randrange(0, 255), 255]):
-        if colorType[0] == 'random':
-            newPixelData = []
-            image = Image.open(self.imageDirCurrent)
-            image.convert('RGBA')
-            pixelData = image.getdata()
-            nextColor = tuple(rgba)
-            for item in pixelData:
-                if item[3] != 0:
-                    newPixelData.append(nextColor)
+            if self.color_type == 'constant':
+                if self.constant_color:
+                    return
                 else:
-                    newPixelData.append(item)
+                    self.constant_color = True
+                    rgba = self.rgba
+            else:
+                rgba = (randrange(0, 256), randrange(0, 256), randrange(0, 256), 255)
+            new_pixel_data = []
+            image = self.current_image
+            image.convert('RGBA')
+            pixel_data = image.getdata()
+            for item in pixel_data:
+                if item[3] != 0:
+                    new_pixel_data.append(rgba)
+                else:
+                    new_pixel_data.append(item)
+            image.putdata(new_pixel_data)
+            self.current_image = image
+        image = self.current_image
+        self.pygame_image = pygame.image.fromstring(image.tobytes(), image.size, image.mode)
 
-            image.putdata(newPixelData)
-            image.save(self.imageDirCurrent)
-            self.imageDisplayed = pygame.image.load(self.imageDirCurrent)
+    def draw(self):
+        self.screen.blit(self.pygame_image, self.rect)
 
-        elif colorType[0] == 'preset':
-            self.PickImage()
+    def move(self):
+        self.rect.left += self.speed[0]
+        self.rect.top += self.speed[1]
+        if self.rect.left < 0 or self.rect.right > display_width:
+            self.speed[0] = -self.speed[0]
+            self.change_color()
+        if self.rect.top < 0 or self.rect.bottom > display_height:
+            self.speed[1] = -self.speed[1]
+            self.change_color()
 
-    def DrawImage(self, screen):
-        screen.blit(self.imageDisplayed, self.imageRect)
-        
-    def MoveImage(self):
-        self.x += self.imageSpeed[0]
-        self.y += self.imageSpeed[1]
-        self.imageRect.top = self.y
-        self.imageRect.left = self.x
-        if self.imageRect.left < 0 or self.imageRect.right > width:
-            self.imageSpeed[0] = -self.imageSpeed[0]
-            self.ChangeColor(colorType, imageType)
 
-        if self.imageRect.top < 0 or self.imageRect.bottom > height:
-            self.imageSpeed[1] = -self.imageSpeed[1]
-            self.ChangeColor(colorType, imageType)
+if not ignore_config_file:
+    try:
+        with open(config_file_path, 'r') as config_file:
+            config = json.load(config_file)
+        image_count = int(config['image_count'])
+        style = str(config['style'])
+        image_size = (int(config['image_size']['width']), int(config['image_size']['height']))
+        color_type = str(config['color_type'])
+        transparency_inversion = 'inverted' if bool(config['transparency_inversion']) else 'normal'
+        rgba = (int(config['image_color']['r']), int(config['image_color']['g']), int(config['image_color']['b']), int(config['image_color']['a']))
+        background_color = (int(config['background_color']['r']), int(config['background_color']['g']), int(config['background_color']['b']))
+        refresh_speed = int(config['refresh_speed'])
+        speed_type = str(config['image_speed']['type'])
+        speed = [float(config['image_speed']['x']), float(config['image_speed']['y'])]
+    except Exception:
+        pass
 
 pygame.init()
 try:
-    config = json.load(open('config.json', 'r'))
-    numberOfImages = int(config['number_of_images'])
-    imageType = config['image_type']
-    imageSize = [int(config['image_size']['width']), int(config['image_size']['height'])]
-    colorType = [config['image_color']['type'], config['image_color']['invert']]
-    rgba = [int(config['image_color']['r']), int(config['image_color']['g']), int(config['image_color']['b']), int(config['image_color']['a'])]
-    backgroundColor = (int(config['background_color']['r']), int(config['background_color']['g']), int(config['background_color']['b']))
-    refreshSpeed = 1 / int(config['refresh_speed'])
-    speedType = [config['image_speed']['type'], int(config['image_speed']['x']), int(config['image_speed']['y'])]
-    width, height = int(config['screen_size']['width']), int(config['screen_size']['height'])
-
-except:
+    display_size = windll.user32.GetSystemMetrics(78), windll.user32.GetSystemMetrics(79)
+except Exception:
     pass
-
-displaySize = windll.user32.GetSystemMetrics(78), windll.user32.GetSystemMetrics(79)
-width = displaySize[0]
-height = displaySize[1]
-imageSize[0] = imageSize[0] / 1920 * displaySize[0]
-imageSize[1] = imageSize[1] / 1080 * displaySize[1]
+display_width, display_height = display_size
+image_size = (image_size[0] / 1920 * display_width, image_size[1] / 1080 * display_height)
+speed = [speed[0] / refresh_speed * 60, speed[1] / refresh_speed * 60]
+screen = pygame.display.set_mode(display_size, pygame.FULLSCREEN)
 clock = pygame.time.Clock()
+images = [ScreensaverImage(screen, style, image_size, speed_type, speed, color_type, transparency_inversion, rgba) for i in range(image_count)]
 pygame.mouse.set_visible(False)
-screen = pygame.display.set_mode(displaySize)
-images = []
-for item in range(0, numberOfImages):
-    images.append(image())
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-            pygame.mouse.set_visible(True)
-            sys.exit(0)
-
-    screen.fill(backgroundColor)
-    for item in images:
-        item.MoveImage()
-        item.DrawImage(screen)
-
-    pygame.display.flip()
-    time.sleep(refreshSpeed)
+run = True
+try:
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+                break
+        screen.fill(background_color)
+        for image in images:
+            image.move()
+            image.draw()
+        pygame.display.flip()
+        time.sleep(1 / refresh_speed)
+finally:
+    pygame.mouse.set_visible(True)
+    raise sys.exit()
--- readme.md
+++ readme.md
@@ -3,38 +3,30 @@ By [LemonPi314](https://github.com/LemonPi314)
 
 Bouncing DVD logo screensaver for Windows.
 ## Credit
-Example project from https://repl.it/  
-Author: Edwin Kofler (eankeen).  
-Website: https://repl.it/talk/learn/A-Starter-Guide-to-Pygame/11741  
-Images from Google Images and the example project.
+Example project by Edwin Kofler (eankeen) from https://repl.it/talk/learn/A-Starter-Guide-to-Pygame/  
+Images from Google Images and the example project
 ## Requirements
 - Windows operating system
-- 25 MB of free space if you are using the `.exe` or `.scr` file
-- 85 MB of free space for temporary files if you are using the `.exe` or `.scr` file
+- 15 MB of free space if you are using the `.exe` or `.scr` file
+- 17 MB of free space for temporary files if you are using the `.exe` or `.scr` file
 ## Usage
 ### `.exe` File
-Download the `dvd-screensaver.exe` file from the `dist` directory and run it from any directory of your choice.
-The `.exe` file is only dependent on temporary files it creates which get deleted when the program exits.
-You may safely delete all other files that you downloaded.
+Download the `dvd-screensaver.exe` file from the [latest release](https://github.com/LemonPi314/dvd-screensaver/releases/latest) and run it from any directory with this command: `./dvd-screensaver.exe -s`. The `.exe` file is only dependent on temporary files it creates which get deleted when the program exits.
 ### `.scr` File
-Make sure you have administrator permissions on your pc.
-Download the screensaver file `dvd-screensaver.scr` from the `dist` directory and move it to `C:\Windows\System32`.
-Right click on it and select "Install"  
-In the screensaver settings menu select "dvd-screensaver".
+Make sure you have administrator permissions on your PC.
+Download the screensaver file `dvd-screensaver.scr` from the [latest release](https://github.com/LemonPi314/dvd-screensaver/releases/latest) and move it to `C:\Windows\System32`. Right click on it and select "Install". In the screensaver settings menu select "dvd-screensaver".
 ### `.py` File
 #### Requirements
 - [Python 3.9](https://www.python.org/downloads/) or higher
 - [pygame](https://pypi.org/project/pygame/)
 - [Pillow](https://pypi.org/project/Pillow/)
+- [PySimpleGUI](https://pypi.org/project/PySimpleGUI/)
 #### Executing
-Locate the file `dvd-screensaver.py` and double click to run it in the Python interpreter.  
-To run the Python script only the `dvd-screensaver` subdirectory is needed.  
-In order for the Python script to work the `images` directory must be in the same folder as the Python script.  
-The folder which contains the script must be named `dvd-screensaver`.
+Download the source code from the [latest release](https://github.com/LemonPi314/dvd-screensaver/releases/latest) and unzip the downloaded file. Open a terminal window and navigate to the unzipped directory. Run this command to start the program: `python dvd-screensaver.py -s`. In order for the Python script to work the `images` directory must be in the same folder as the Python script. The directory structure should look like this.
 ```
 dvd-screensaver
 │   dvd-screensaver.py
-│   dvd-screensaver.pyproj
+│   config.py
 │   
 └───images
     │
@@ -42,10 +34,23 @@ dvd-screensaver
     │   │   ...
     │
     └───2
-    │   │   ...
-    │
-    └───current-image
-        │   current-image.png
+        │   ...
 ```
+### Configuration
+To open the configuration menu run the Python script, `.exe` file, or `.scr` file without the `-s` argument. The `-s` argument is required to run the screensaver without opening the configuration menu, see the [Microsoft Docs](https://docs.microsoft.com/en-us/troubleshoot/windows/win32/screen-saver-command-line) for more details. The configuration JSON file is saved in `%APPDATA%/LemonPi314/dvd-screensaver/config.json`.
+#### Configuration Options
+Option|Description
+------|-----------
+Number of images|Number of items bouncing around on screen
+Image style|Style of image, can be either '1' or '2'
+Refresh speed|Refresh speed of the display loop in updates per second. Values higher than 120 may cause it to not function properly
+Image width, height|The maximum width and height of the image in pixels
+Image speed type|Speed type, can be 'random' or 'constant'
+Image speed x, y|If the speed type is set to 'constant' the integers are the x and y speed of the image in pixels. If the speed type is set to 'random' the integers are the range for the speed to be chosen from
+Image color type|Color type, can be 'preset', 'random', or 'constant'
+Image red, green, blue, alpha|RGBA value for the image(s) if the color type is set to 'constant'
+Invert transparency|Transparency inversion, colored areas of the image become clear and vice versa
+Background red, green, blue|RGB color of the background
+
 ## License
 [MIT License](https://choosealicense.com/licenses/mit/)
