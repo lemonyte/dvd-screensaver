6fc2bfb9c9d97900a23fe4ea35c4b9707a745c2f
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-02-14 10:07:35 -0800

    Refactor and cleanup

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,10 @@ fn main() {
     nannou::app(model).update(update).run();
 }
 
+fn change_color(image: &image::DynamicImage) -> image::DynamicImage {
+    image.huerotate(thread_rng().gen_range(120..240))
+}
+
 fn model(app: &App) -> Model {
     let primary_window_id = app
         .new_window()
@@ -27,13 +31,10 @@ fn model(app: &App) -> Model {
     primary_window.set_cursor_visible(false);
 
     let img_data = include_bytes!("../assets/dvd_logo.png");
-    let image = image::load_from_memory(img_data)
-        .unwrap()
-        .thumbnail(
-            app.window_rect().w() as u32 / 2,
-            app.window_rect().h() as u32 / 2,
-        )
-        .huerotate(thread_rng().gen_range(120..240));
+    let image = change_color(&image::load_from_memory(img_data).unwrap().thumbnail(
+        app.window_rect().w() as u32 / 6,
+        app.window_rect().h() as u32 / 6,
+    ));
     let rect = Rect::from_x_y_w_h(
         0.0,
         0.0,
@@ -51,38 +52,40 @@ fn model(app: &App) -> Model {
 }
 
 fn window_event(app: &App, model: &mut Model, event: WindowEvent) {
-    match event {
-        WindowEvent::MouseMoved(pos) => {
-            if model.m_pos.is_none() {
-                model.m_pos = Some(pos)
+    if app.time > 0.1 {
+        match event {
+            WindowEvent::MouseMoved(pos) => {
+                if model.m_pos.is_none() {
+                    model.m_pos = Some(pos)
+                }
+                if model.m_pos.unwrap() != pos {
+                    app.quit()
+                }
             }
-            if app.time > 0.1 && model.m_pos.unwrap() != pos {
-                app.quit()
-            }
+            WindowEvent::MousePressed(..) => app.quit(),
+            WindowEvent::KeyPressed(..) => app.quit(),
+            WindowEvent::MouseWheel(..) => app.quit(),
+            _ => (),
         }
-        WindowEvent::MousePressed(..) => app.quit(),
-        WindowEvent::KeyPressed(..) => app.quit(),
-        WindowEvent::MouseWheel(..) => app.quit(),
-        _ => (),
     }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
     let win = app.window_rect();
+    let delta_time = app.duration.since_prev_update.secs() as f32;
     let dvd_vel = &mut model.dvd_vel;
     model.dvd_rect = model
         .dvd_rect
-        .shift_x(dvd_vel.x * app.duration.since_prev_update.secs() as f32)
-        .shift_y(dvd_vel.y * app.duration.since_prev_update.secs() as f32);
-    let rect = model.dvd_rect;
+        .shift_x(dvd_vel.x * delta_time)
+        .shift_y(dvd_vel.y * delta_time);
 
-    if rect.left() <= win.left() || rect.right() >= win.right() {
+    if model.dvd_rect.left() <= win.left() || model.dvd_rect.right() >= win.right() {
         dvd_vel.x = -dvd_vel.x;
-        model.image = model.image.huerotate(thread_rng().gen_range(120..240));
+        model.image = change_color(&model.image);
     }
-    if rect.bottom() <= win.bottom() || rect.top() >= win.top() {
+    if model.dvd_rect.bottom() <= win.bottom() || model.dvd_rect.top() >= win.top() {
         dvd_vel.y = -dvd_vel.y;
-        model.image = model.image.huerotate(thread_rng().gen_range(120..240));
+        model.image = change_color(&model.image);
     }
 }
 
